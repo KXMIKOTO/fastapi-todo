fd86d095e84dcb00cf5465c2c4d621e483b7b6bc
KXMIKOTO <kxmikoto@gmail.com>
2025-05-23 19:24:15 +0300

    Добавил алерты на страрицу если возникает ошибка

--- todo-frontend/src/pages/TodoPage.jsx
+++ todo-frontend/src/pages/TodoPage.jsx
@@ -5,30 +5,68 @@ import TaskForm from "../components/TaskForm";
 
 export default function TodoPage() {
     const [tasks, setTasks] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        getTasks().then(setTasks);
+        async function fetchTasks() {
+            try {
+                setLoading(true);
+                const data = await getTasks();
+                setTasks(data);
+            } catch (err) {
+                setError("error fetching tasks");
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchTasks();
     }, []);
 
     const handleCreate = async (task) => {
-        const newTask = await createTask(task);
-        setTasks([...tasks, newTask]);
+        try {
+            const newTask = await createTask(task);
+            setTasks(prev => [...prev, newTask]);
+        }
+        catch (err) {
+            alert("error creating task");
+        }
     };
 
     const handleUpdate = async (id, updates) => {
-        const updatedTask = await updateTask(id, updates);
-        setTasks(tasks.map(task => (task.id === id ? updatedTask : task)));
+        try {
+            const updatedTask = await updateTask(id, updates);
+            setTasks(prev => prev.map(task => (task.id === id ? updatedTask : task)));
+        } 
+        catch (err) {
+            alert("error updating task");
+        }
     };
 
     const handleDelete = async (id) => {
-        await deleteTask(id);
-        setTasks(tasks.filter(task => task.id !== id));
+        try{
+            await deleteTask(id);
+            setTasks(prev => prev.filter(task => task.id !== id));
+        }
+        catch (err) {
+            alert("error deleting task");
+        }
     };
+
+    if (loading) {
+        return <div className="text-center mt-10">Loading...</div>;
+    }
+    if (error) {
+        return <div className="text-center mt-10 text-red-600">{error}</div>;
+    }
 
     return (
         <div className="max-w-md mx-auto mt-10">
             <h1 className="text-2xl font-bold mb-4">Todo List</h1>
             <TaskForm onCreate={handleCreate} />
+            {tasks.length === 0 && (
+                <div className="text-center mt-4 text-gray-600">No tasks available</div>
+            )}
             {tasks.map(task => (
                 <TaskItem 
                     key={task.id} 
